2a0cc583c30ab2a53aeca4f9cd44c19c5bf114e4
Jose117053 <adolfo19420@gmail.com>
2024-02-26 23:49:03 -0600

    El compilador ya dejo de chillar

--- Practica2.c
+++ Practica2.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
+#include<math.h>
+#include<string.h> // Include string.h for string functions
+#include <stdlib.h>
 
-int main(int argc, char *argv[]){
 
-    
-    return 0;
-}
 
 
 //Una funci ́on que calcule la media aritm ́etica un areglo de datos, recibe el
@@ -36,9 +35,37 @@ float media_armonica(float datos[], int n){
 float media_geometrica(float datos[], int n){
 
     float producto=1;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++){
         producto*=datos[i];
+        printf("i: %d dato: %f, producto: %f\n", i, datos[i],producto);
+    }
 
-    return pow(producto, 1/n);
+   
+    return pow(producto, 1.0/n);
 }
 
+void uso(){
+    printf("Uso: Practica2 L L L\n");
+    printf("Donde L puede ser la letra A,H,G\n");
+    printf("Cada entrada L puede ser distinto\n");
+}
+
+void banderas(char* hola){
+    printf("esto es el apuntador: %c \n",*hola);
+}
+
+int main(int argc, char *argv[]){
+
+    
+    if(argc <3 ||  strcmp(argv[1], "Practica2") != 0)
+        uso();
+
+    float datos[argc-3];
+
+    for(int i=3; i<argc;i++)
+        datos[i-3]=(float) atof(argv[i]);
+
+    
+    printf("%f", media_geometrica(datos,argc-3));
+    return 0;
+}
